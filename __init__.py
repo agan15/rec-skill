b9ac70bdbeff2f2cfb57843794682a9eab416df1
Aasta <aastagandhi@gmail.com>
2017-08-01 18:17:17 -0700

    updated init

--- __init__.py
+++ __init__.py
@@ -71,52 +71,50 @@ def top_search(food):
     return client.recipe(r.ix[0, "ID"])
 
 class RecipeSkill(MycroftSkill):
-    
-   
-    
+
     #constructor
     def __init__ (self):
         super(RecipeSkill, self).__init__(name="RecipeSkill") 
-        self.rec_name = ""
-        self.ask = False
-        
-       # self.set_context(self, "food")
+
     
     def initialize(self):
-       # keyword_intent = IntentBuilder("RecipeKeywordIntent").\
-           # require("RecipeKeyword").optionally("recipe").build()
-        keyword_intent = IntentBuilder("RecipeKeywordIntent").\
-            require("RecipeKeyword").require("recipe").build()
-        needMore_intent = IntentBuilder("FoodTypeIntent").\
-            require("FoodType").build()
-        no_intent = IntentBuilder("NoIntent").\
-            require("NoKeyword").build()
-      
+        self.register_vocabulary("RecipeKeyword", "recipe")
+
+        keyword_intent = IntentBuilder("RecipeKeywordIntent").require("RecipeKeyword").require("recipe").build()
         self.register_intent(keyword_intent, self.handle_recipeKeyword)
+       
+        needMore_intent = IntentBuilder("FoodTypeIntent").require("FoodType").build()
         self.register_intent(needMore_intent, self.handle_needMore)
+       
+        no_intent = IntentBuilder("NoIntent").require("NoKeyword").build()
         self.register_intent(no_intent, self.handle_no)
+
+        print("initialized")
         
     def handle_recipeKeyword(self, message):
         #GET FIRST SEARCH
+        print("three")
+        self.set_context("FoodContext") 
         rec_name = message.data.get("recipe", None)
-       # self.adds_context(rec_name)
+        self.adds_context(rec_name)
+        print("four")
         self.speak("Searching for recipe ingredients")
         rec = get_recipes(rec_name)
         #RESPOND, ASK SECOND SEARCH
-        self.ask = True
+        
         self.speak(str(rec.name) + " has a rating of" + str(rec.rating) + 
         " and it takes " + str(rec.totalTime) + "to make. Anything else?", 
-        expect_response = True)       
+        expect_response = True)     
+         
         
     def handle_needMore(self, message):
-        if self.ask:
-            self.rec_name = self.rec_name + message.data.get("food_descrip", None)
-            rec = get_recipes(self.rec_name)
-            self.speak("rating is" + self.rec_name +"is" + str(rec.rating))
+        self.rec_name = self.rec_name + message.data.get("food_descrip", None)
+        rec = get_recipes(self.rec_name)
+        self.speak("rating is" + self.rec_name +"is" + str(rec.rating) + "anything else?", expect_response = True)
         
     def handle_no(self, message):
-        if self.ask:
-            self.speak("Have a good meal")
+        self.speak("Have a good meal")
+        self.remove_context("FoodContext")
         
        
 #       for ingred in rec:
